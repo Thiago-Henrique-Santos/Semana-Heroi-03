4f718874827d67a9494e25207f8194f5a0c49992
Thiago Henrique dos Santos <72514176+Thiago-Henrique-Santos@users.noreply.github.com>
2024-01-30 14:09:42 -0300

    fix: :bug: Corrigindo bug de últimas cameras ficarem cortadas
    Ajustando tamanho das câmeras, conforme o tamanho da tela.

--- frontend/src/app/room/[id]/page.tsx
+++ frontend/src/app/room/[id]/page.tsx
@@ -7,21 +7,21 @@ export default function Room({params}: {params: {id: string}}){
         <div className="h-screen">
             <Header/>
             <div className="flex h-[70%]">
-                <div className="md:w-[85%] w-full m-3">
+                <div className="md:w-[85%] w-full m-5">
                     <div className="grid md:grid-cols-2 grid-cols-1 gap-2">
-                        <div className="bg-gray-950 w-full rounded-md h-full p-2 relative">
+                        <div className="bg-gray-950 w-[85%] rounded-md h-[85%] p-2 relative">
                             <video className="h-full w-full"></video>
                             <span className="absolute bottom-3">Thiago Santos</span>
                         </div>
-                        <div className="bg-gray-950 w-full rounded-md h-full p-2 relative">
+                        <div className="bg-gray-950 w-[85%] rounded-md h-[85%] p-2 relative">
                             <video className="h-full w-full"></video>
                             <span className="absolute bottom-3">Matheus Santos</span>
                         </div>
-                        <div className="bg-gray-950 w-full rounded-md h-full p-2 relative">
+                        <div className="bg-gray-950 w-[85%] rounded-md h-[85%] p-2 relative">
                             <video className="h-full w-full"></video>
                             <span className="absolute bottom-3">Yuri Souza</span>
                         </div>
-                        <div className="bg-gray-950 w-full rounded-md h-full p-2 relative">
+                        <div className="bg-gray-950 w-[85%] rounded-md h-[85%] p-2 relative">
                             <video className="h-full w-full"></video>
                             <span className="absolute bottom-3">Pedro Lemos</span>
                         </div>
